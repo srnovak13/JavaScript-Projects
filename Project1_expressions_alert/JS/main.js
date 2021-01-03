78800a6ae7c9c8c5b7eeb79a67bbd70bd11a3539
srnovak13 <srnovak@gmail.com>
2021-01-02 18:58:20 -0800

    Add first JS project

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+
+document.write ('Hello, World!');
+var A='This is a string';
+document.write(A);
+var B="Warning! About to blow!";//Assigning value to variable B
+window.alert(B);
+document.write ("There was a loud \"Thud\" that came from the basement steps."
++"The basement door creaked open and a low growl could be heard."
++"The girls ran out of the house as fast as they could!");//concatenating strings
+var C="The basement door flew open yet nothing was there";
+document.write (C)
+var Characters="The Lovecrafts", Daughter="Sofia", Daughter2="Isabella", Son="Edgar Allan", Father="H.P"
+document.write(Son)
+var Daughter= Daughter.fontcolor("pink");
+var Daughter2= Daughter2.fontcolor("red");
+var Son= Son.fontcolor("blue");
+var Father= Father.fontcolor("green");
+document.write(Daughter2);
+document.write(5+5);
+function My_First_Function() {//Naming the function
+    var str="This text is green!";//Naming the variable
+    var result=str.fontcolor("green");//Using fontcolor method of var=str
+    document.getElementById("Green_Text").innerHTML=result;//putting the value of 
+                                                        //result into Green_Text element
+
+
+}
