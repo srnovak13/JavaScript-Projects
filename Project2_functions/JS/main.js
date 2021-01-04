8c88c63132a6578197b4b84440bf79320edb09d9
srnovak13 <srnovak@gmail.com>
2021-01-04 13:01:21 -0800

    Add Project2_functions file

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+
+
+function My_First_Function() { //naming function
+    var str="This text is green!";//assigning value to variable
+    var result=str.fontcolor("green");//assigning font color to variable str
+    document.getElementById("Green_Text").innerHTML=result;
+}
+
+function My_Function() {//naming function
+    var sentence="I am learning";//assigning value to variable
+    sentence +="a lot from this book";//concatenating strings
+    document.getElementById("concatenate").innerHTML=sentence;//putting value of sentence into "concatenate" element
+}
