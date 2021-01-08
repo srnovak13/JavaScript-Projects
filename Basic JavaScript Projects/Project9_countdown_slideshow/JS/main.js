5bfbabd758f6b436207930a027228adaeed72b81
srnovak13 <srnovak@gmail.com>
2021-01-08 15:19:39 -0800

    Add Project9

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -16,7 +16,8 @@ showSlides(slideIndex);
 
 //NEXT/PREVIOUS CONTROLS
 function plusSlides(n) {
-    showSlides(slideIndex+=n);
+    showSlides(slideIndex+=n); 
+    
 }
 
 //THUMBNAIL IMAGE CONTROLS
@@ -27,15 +28,18 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides=document.getElementsByClassName("mySlides");
+    
     var dots=document.getElementsByClassName("dot");
     if (n>slides.length) {slideIndex= 1}
-    if (n<1) {slideIndex=slides.lenghth}
+    if (n<1) {slideIndex=slides.length}
     for (i=0; i<slides.length;i++) {
         slides[i].style.display="none";
     }
     for (i=0; i<dots.length; i++){
         dots[i].className=dots[i].className.replace("active","");
+    
     }
+    
     slides[slideIndex-1].style.display="block";
     dots[slideIndex-1].className+="active";
 }
