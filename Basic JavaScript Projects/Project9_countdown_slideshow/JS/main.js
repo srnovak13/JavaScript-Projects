f8ce8f8daa8b97768da827f5e93b47b1ffd9e404
srnovak13 <srnovak@gmail.com>
2021-01-08 12:14:20 -0800

    Commit all changes

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,5 +1,5 @@
 function countdown() {
-    var Seconds=document.getElementById("seconds").value;
+    var seconds=document.getElementById("seconds").value;
         function tick() {
         seconds=seconds-1;
         timer.innerHTML=seconds;
@@ -11,3 +11,31 @@ function countdown() {
     tick();
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//NEXT/PREVIOUS CONTROLS
+function plusSlides(n) {
+    showSlides(slideIndex+=n);
+}
+
+//THUMBNAIL IMAGE CONTROLS
+function currentSlide(n) {
+    showSlides(slideIndex=n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides=document.getElementsByClassName("mySlides");
+    var dots=document.getElementsByClassName("dot");
+    if (n>slides.length) {slideIndex= 1}
+    if (n<1) {slideIndex=slides.lenghth}
+    for (i=0; i<slides.length;i++) {
+        slides[i].style.display="none";
+    }
+    for (i=0; i<dots.length; i++){
+        dots[i].className=dots[i].className.replace("active","");
+    }
+    slides[slideIndex-1].style.display="block";
+    dots[slideIndex-1].className+="active";
+}
