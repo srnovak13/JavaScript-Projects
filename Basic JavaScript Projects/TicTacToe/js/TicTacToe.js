d9a2b68750d5a2f74b608d2a8019d797d9fd028e
srnovak13 <srnovak@gmail.com>
2021-01-15 16:18:03 -0800

    Correct Line 196

--- Basic JavaScript Projects/TicTacToe/js/TicTacToe.js
+++ Basic JavaScript Projects/TicTacToe/js/TicTacToe.js
@@ -193,7 +193,7 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
             if (x1 <= x2 && y1 >= y2) {
                 if (x < x2) {x += 10;}
                 if (y > y2) {y -= 10;}
-                if (x >= x2 && y <= y2) { cancelAnimationFrame(animationloop);}
+                if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop);}
             }
         }
         //This function clear our canvas after our win line is drawn.
